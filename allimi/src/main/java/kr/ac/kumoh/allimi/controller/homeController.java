8cab51f12805fe04c22b4fc525d4e43ebec4fae5
Juhyorim <joohr1234@gmail.com>
2023-03-31 22:43:15 +0900

    apply .gitignore

--- allimi/src/main/java/kr/ac/kumoh/allimi/controller/homeController.java
+++
@@ -1,4 +0,0 @@
-package kr.ac.kumoh.allimi.controller;
-
-//public class homeController {
-//}
--- allimi/src/main/java/kr/ac/kumoh/allimi/domain/Role.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kumoh.allimi.domain;
-
-import jakarta.persistence.Enumerated;
-
-public enum Role {
-    PROTECTOR, WORKER, MANAGER
-}
