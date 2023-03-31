e397fe7e6711d8473a67a4108da4eed8e60cdb33
Juhyorim <joohr1234@gmail.com>
2023-03-31 20:32:30 +0900

    add all

---
+++ allimi/src/test/java/kr/ac/kumoh/allimi/AllimiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.allimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
